600257e9d05632902ba510359d793a5877a9c824
Sean Metzgar <sean.metzgar@gmail.com>
2019-02-26 13:37:27 -0500

    Attempting to disable the button temporarily for download

--- js/src/functions/function.downloadAvatar.js
+++ js/src/functions/function.downloadAvatar.js
@@ -4,35 +4,41 @@ function downloadAvatar() {
     $button = $(this),
     $link = null;
   html2canvas(faceHtml, {logging: false, backgroundColor: null}).then(function(canvas) {
-      var imageData = null;
-      try {
-        imageData = canvas.toDataURL("image/png").replace("data:image/png;base64,", "");
-      } catch(e) {
-        imageData = null;
-        console.error("Error: Tainted Canvas... Are you running locally?");
-        alert('Error: Are you running locally?');
-      }
-
-      if (imageData !== null) {
-        $.ajax({
-          url: 'save_image.php',
-          data: {
-            data: imageData
-          },
-          type: 'post',
-          success: function (response) {
-            var linkHref = null;
-            if (typeof response === "string" && response.indexOf("generated") !== -1) {
-              $('.hiddenLink').empty();
-              linkHref = "download.php?f=" + encodeURIComponent(response);
-              $link = $("<a>download</a>").appendTo(".hiddenLink");
-              $link.attr("href", linkHref)
-                .get(0).click();
-              $link.remove();
-              $link = null;
-            }
-          }
-        });
-      }
+      $('#download').prop("disabled", true);
+      setTimeout(triggerDownload(canvas), 700);
   });
+}
+
+function triggerDownload(canvas) {
+  var imageData = null;
+  try {
+    imageData = canvas.toDataURL("image/png").replace("data:image/png;base64,", "");
+  } catch(e) {
+    imageData = null;
+    console.error("Error: Tainted Canvas... Are you running locally?");
+    alert('Error: Are you running locally?');
+  }
+
+  if (imageData !== null) {
+    $.ajax({
+      url: 'save_image.php',
+      data: {
+        data: imageData
+      },
+      type: 'post',
+      success: function (response) {
+        var linkHref = null;
+        if (typeof response === "string" && response.indexOf("generated") !== -1) {
+          $('.hiddenLink').empty();
+          linkHref = "download.php?f=" + encodeURIComponent(response);
+          $link = $("<a>download</a>").appendTo(".hiddenLink");
+          $link.attr("href", linkHref)
+            .get(0).click();
+          $link.remove();
+          $link = null;
+        }
+      }
+    });
+  }
+  $('#download').prop("disabled", false).removeProp("disabled");
 }
