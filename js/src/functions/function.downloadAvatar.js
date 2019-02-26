558388579adb2df029916e161eca9f1f35af65f4
Sean Metzgar <sean.metzgar@gmail.com>
2019-02-26 09:21:08 -0500

    :bug: Fix: Download as application/octet-stream
    Download now initializes a download instead of opening image in a new tab

--- js/src/functions/function.downloadAvatar.js
+++ js/src/functions/function.downloadAvatar.js
@@ -4,13 +4,10 @@ function downloadAvatar() {
     $button = $(this),
     $link = null;
   html2canvas(faceHtml, {logging: false, backgroundColor: null}).then(function(canvas) {
-      var image = null,
-        imageData = null;
+      var imageData = null;
       try {
-        image = canvas.toDataURL("image/png").replace("image/octet-stream");
         imageData = canvas.toDataURL("image/png").replace("data:image/png;base64,", "");
       } catch(e) {
-        image = null;
         imageData = null;
         console.error("Error: Tainted Canvas... Are you running locally?");
         alert('Error: Are you running locally?');
@@ -24,9 +21,10 @@ function downloadAvatar() {
           },
           type: 'post',
           success: function (response) {
-
+            var linkHref = null;
             if (typeof response === "string" && response.indexOf("generated") !== -1) {
               $('.hiddenLink').empty();
+              linkHref = "download.php?f=" + encodeURIComponent(response);
               $link = $("<a>download</a>").appendTo(".hiddenLink");
               $link.attr("href", response)
                 .get(0).click();
