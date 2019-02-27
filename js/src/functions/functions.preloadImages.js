698cba008ad36c966efa5593516b5a6cd1872a9e
Sean Metzgar <sean.metzgar@gmail.com>
2019-02-27 13:45:12 -0500

    Debug issue with preload: 2

--- js/src/functions/functions.preloadImages.js
+++ js/src/functions/functions.preloadImages.js
@@ -9,17 +9,18 @@ function preloadImages() {
     url: 'xhrImageList.php',
     success: function(response) {
       var files = [];
-      var tempImage = null;
-      var key = 0;
+      var loaderKey = 0;
       if (typeof response === "object"
       && response.hasOwnProperty("data")
       && response.data instanceof Array
       && response.data.length > 0) {
         files = response.data;
-        for (file in files) {
-          key = preloader.push(new Image()) - 1;
-          preloader[key].onload = registerPreload(key);
-          preloader[key].src = file;
+        for (fileKey in files) {
+          if (typeof files[fileKey] === "string" && files[fileKey].length > 0) {
+            loaderKey = preloader.push(new Image()) - 1;
+            preloader[loaderKey].onload = registerPreload(loaderKey);
+            preloader[loaderKey].src = files[fileKey];
+          }
         }
       }
     }
