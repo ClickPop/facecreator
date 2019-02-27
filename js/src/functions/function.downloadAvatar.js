2bc1a8f953f306cdaa29d29bc537c7e3f982f6ea
Sean Metzgar <sean.metzgar@gmail.com>
2019-02-26 19:27:47 -0500

    Save image to generated directory for later use

--- js/src/functions/function.downloadAvatar.js
+++ js/src/functions/function.downloadAvatar.js
@@ -23,6 +23,25 @@ function downloadAvatar() {
         alert('Error: Are you running locally?');
       }
 
+      saveImage();
+
       $('#download').prop("disabled", false).removeProp("disabled");
   });
+}
+
+function saveImage(canvas) {
+  var imageData = null;
+  try {
+    imageData = canvas.toDataURL("image/png").replace("data:image/png;base64,", "");
+  } catch(e) {}
+
+  if (imageData !== null) {
+    $.ajax({
+      url: 'save_image.php',
+      data: {
+        data: imageData
+      },
+      type: 'post'
+    });
+  }
 }
