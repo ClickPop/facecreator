8b7a9615ccfb6bff7d5279c92a26f76d59207c35
Sean Metzgar <sean.metzgar@gmail.com>
2019-02-26 19:11:00 -0500

    Fix: Re-enable download button after download

--- js/src/functions/function.downloadAvatar.js
+++ js/src/functions/function.downloadAvatar.js
@@ -3,67 +3,26 @@ function downloadAvatar() {
     $faceImage = $('#faceImage').empty(),
     $button = $(this),
     $link = null;
-  html2canvas(faceHtml, {logging: false, backgroundColor: null}).then(function(canvas) {
+  html2canvas(faceHtml, {logging: false, allowTain: true, backgroundColor: null}).then(function(canvas) {
       $('#download').prop("disabled", true);
-      setTimeout(triggerDownload(canvas), 700);
-  });
-}
 
-function triggerDownload(canvas) {
-  try {
-    canvas.toBlob(function(blob) {
-      var $link = $("<a>download</a>")
-        .appendTo(".hiddenLink")
-        .attr("href", "#")
-        .on("click", function(e) {
-          e.preventDefault();
-          saveAs(blob, "FriendlyFace.png")
-        }).trigger("click");
-      $link.remove();
-      $link = null;
-    });
-  } catch(e) {
-    console.error("Error: Tainted Canvas... Are you running locally?");
-    alert('Error: Are you running locally?');
-  }
-}
-
-function triggerDownloadOld(canvas) {
-  var imageData = null;
-  try {
-    imageData = canvas.toDataURL("image/png").replace("data:image/png;base64,", "");
-  } catch(e) {
-    imageData = null;
-    console.error("Error: Tainted Canvas... Are you running locally?");
-    alert('Error: Are you running locally?');
-  }
-
-  if (imageData !== null) {
-    $.ajax({
-      url: 'save_image.php',
-      data: {
-        data: imageData
-      },
-      type: 'post',
-      success: function (response) {
-        var linkHref = null;
-        if (typeof response === "string" && response.indexOf("generated") !== -1) {
-          $('.hiddenLink').empty();
-          linkHref = "download.php?f=" + encodeURIComponent(response);
-          $link = $("<a>download</a>").appendTo(".hiddenLink");
-          $link.attr("href", linkHref)
-            .get(0).click();
+      try {
+        canvas.toBlob(function(blob) {
+          $link = $("<a>download</a>")
+            .appendTo(".hiddenLink")
+            .attr("href", "#")
+            .on("click", function(e) {
+              e.preventDefault();
+              saveAs(blob, "FriendlyFace.png")
+            }).trigger("click");
           $link.remove();
           $link = null;
-        }
+        });
+      } catch(e) {
+        console.error("Error: Tainted Canvas... Are you running locally?");
+        alert('Error: Are you running locally?');
       }
-    });
-  }
-  $('#download').prop("disabled", false).removeProp("disabled");
+
+      $('#download').prop("disabled", false).removeProp("disabled");
+  });
 }
-
-
-html2canvas(document.getElementById('faceContainer'), {logging: false, backgroundColor: null}).then(function(canvas) {
-    console.log(canvas);
-    canvas.toBlob(function(blob) {wow = blob;});
-});
